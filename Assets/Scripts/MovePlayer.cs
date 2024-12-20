c1c370fe5ee0b59ba492edbe2d129e8a6df18753
Oliver Creese <olicreese@gmail.com>
2024-12-20 17:21:02 +0000

    epic commit

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
@@ -6,7 +7,8 @@ public class MovePlayer : MonoBehaviour
     float horizontalInput;
     float verticalInput;
     bool facingRight;
-    bool isJab;
+    bool isAttack;
+    bool isDash;
 
     Animator anim;
     private void Awake()
@@ -20,36 +22,61 @@ public class MovePlayer : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         anim.SetFloat("Speed", Mathf.Abs(horizontalInput != 0 ? horizontalInput : verticalInput));
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            isAttack = true;
+            anim.SetTrigger("isJab");
+        }
+    
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            isJab = true;
-            anim.SetBool("isJab", isJab);
+            isDash = true;
+            anim.SetTrigger("isDash");
+            onDash();
         }
-        else if (Input.GetKeyUp(KeyCode.Space)) isJab = false;
+        else if (Input.GetKeyUp(KeyCode.Space)) isDash = false;
 
-        if (horizontalInput != 0 && !isJab)
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isAttack = true;
+            anim.SetTrigger("isKICK");
+        }
+        
+        
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.5f && !anim.IsInTransition(0)) isAttack = false;
+
+
+        if (horizontalInput != 0 && !isAttack)
         {
             anim.SetBool("isRunning", true);
         }
         else anim.SetBool("isRunning", false);
         
-        if (horizontalInput == 0 && verticalInput != 0 && !isJab)
+        if (horizontalInput == 0 && verticalInput != 0 && !isAttack)
         {
             anim.SetBool("isShuffle", true);
         }
         else anim.SetBool("isShuffle", false);
-       
+
     }
 
+    
     private void FixedUpdate()
     {
 
-        if ((horizontalInput != 0 || verticalInput != 0) && !isJab)
+        if ((horizontalInput != 0 || verticalInput != 0) && !isAttack)
         {
             Vector3 movement = new Vector3(horizontalInput * runSpeed, verticalInput * runSpeed, 0);
             transform.position = transform.position + movement * Time.deltaTime;
         }
         Flip(-horizontalInput);
+    }
+    
+
+    private void onDash()
+    {
+        Vector3 dashVector = new Vector3(horizontalInput * runSpeed *  600, 0,0);
+        transform.position = transform.position + dashVector * Time.deltaTime;
     }
 
     private void Flip(float horizontal)
